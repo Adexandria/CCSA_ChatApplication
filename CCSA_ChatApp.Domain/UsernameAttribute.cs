4c0d044022e38289e652bfa48531a30742420d67
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-15 17:30:52 +0100

    Created attribute for password and username

---
+++ CCSA_ChatApp.Domain/UsernameAttribute.cs
@@ -0,0 +1,26 @@
+﻿using CCSA_ChatApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSA_ChatApp.Domain
+{
+    public class UsernameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+          /*  //Get dependency injection of user
+            string username = value.ToString();
+            //Get existing user
+            *//*User currentUser = _user.GetUser(username).Result;*//*
+            if (currentUser == null)
+            {
+                return ValidationResult.Success;
+            }*/
+            return new ValidationResult("Username already exist");
+        }
+    }
+}
