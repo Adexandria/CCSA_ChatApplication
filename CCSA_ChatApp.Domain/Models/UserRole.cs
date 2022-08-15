d55fa3a04ab0af995c830c2311508b0b35c5cf6b
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-15 16:40:46 +0100

    create models for refresh token and user role

---
+++ CCSA_ChatApp.Domain/Models/UserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace CCSA_ChatApp.Domain.Models
+{
+    public class UserRole
+    {
+        public Guid RoleId { get; set; }
+        public string Role { get; set; }
+        public User User { get; set; }
+    }
+}
