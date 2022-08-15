3ea1953bc19e3e9a9c2b626d94024cc7d5c5556b
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-15 17:39:31 +0100

    Added Validation

--- CCSA_ChatApp.Domain/DTOs/LogInDTO.cs
+++ CCSA_ChatApp.Domain/DTOs/LogInDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace CCSA_ChatApp.Domain.DTOs
 {
     public class LogInDTO
     {
+        [Required(ErrorMessage = "Enter Username")]
         public string UserName { get; set; }
+        
+        [Required(ErrorMessage = "Enter Password")]
         public string Password { get; set; }
     }
 }
--- CCSA_ChatApp.Domain/DTOs/SignUpDTO.cs
+++ CCSA_ChatApp.Domain/DTOs/SignUpDTO.cs
@@ -1,23 +1,36 @@
 ﻿using CCSA_ChatApp.Domain.Models;
 using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CCSA_ChatApp.Domain.DTOs
 {
     public class SignUpDTO
     {
-        public Country Country { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
+        [Required(ErrorMessage = "Enter First Name")]
         public string FirstName { get; set; }
+        
         public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Enter Lastname")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Enter Username")]
+        [Username]
+        public string Username { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; }
+
+        public Country Country { get; set; } = Country.Nigeria;
+        
+        [Password,StringLength(7,ErrorMessage = "Password must be more than 4 and less than 7 characters",MinimumLength = 4)]
         public string Password { get; set; }
-        public string ConfirmationPassword { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password and ConfirmPassword do not match")]
+        public string ConfirmPassword { get; set; }
+        
+        
         public IFormFile Picture { get; set; }
+        
     }
 }
