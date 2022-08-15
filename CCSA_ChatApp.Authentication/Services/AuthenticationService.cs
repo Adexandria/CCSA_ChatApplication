c144d3228a238decdc92011ad5eb3e0a30ab45d8
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-15 13:45:28 +0100

    Created authentication service class

---
+++ CCSA_ChatApp.Authentication/Services/AuthenticationService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSA_ChatApp.Authentication.Services
+{
+    public class AuthenticationService
+    {
+        
+        public static void ConfigureServices(IConfiguration config,IServiceCollection service)
+        {
+            IConfiguration _jwtKey = config.GetSection("JWTSettings");
+            service.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
+            AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = _jwtKey.GetSection("validIssuer").Value,
+                    ValidAudience = _jwtKey.GetSection("validAudience").Value,
+                    AuthenticationType = "Bearer",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey.GetSection("securityKey").Value))
+                };
+
+            });
+        }
+
+    }
+}
