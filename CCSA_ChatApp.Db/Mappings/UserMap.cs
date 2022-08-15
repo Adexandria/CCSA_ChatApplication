84be3093610feb0f06dbbf2fe0d37ea71cfc3d08
Iruoma Onyia <Iruomaonyia@outlook.com>
2022-08-15 16:43:16 +0100

    Added mappings for User, MessageHistory and Message models. Made a few name changes in the domain layer

---
+++ CCSA_ChatApp.Db/Mappings/UserMap.cs
@@ -0,0 +1,27 @@
+﻿using CCSA_ChatApp.Domain.Models;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSA_ChatApp.Db.Mappings
+{
+    public class UserMap : ClassMap<User>
+    {
+        public UserMap()
+        {
+            Table("Users");
+            Id(user => user.UserId);
+            Map(user => user.FirstName);
+            Map(user => user.MiddleName);
+            Map(user => user.LastName);
+            Map(user => user.Email);
+            HasMany(user => user.Contacts);
+            HasMany(user => user.Histories);
+            HasMany(user => user.GroupChats);
+
+        }
+    }
+}
