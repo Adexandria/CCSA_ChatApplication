feebc45e25ad74b9de69e7826fb4138b4aff5bbf
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-16 15:30:17 +0100

    added authen service

---
+++ CCSA_ChatApp.Infrastructure/Services/IAuth.cs
@@ -0,0 +1,18 @@
+﻿using CCSA_ChatApp.Domain.DTOs.UserDTOs;
+using CCSA_ChatApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSA_ChatApp.Infrastructure.Services
+{
+    public interface IAuth
+    {
+        Task AddUserRole(UserRole role);
+        IEnumerable<UserRoleDTO> GetUserRole(Guid id);
+        Task SaveRefreshToken(RefreshToken token);
+        Task<RefreshToken> GetExistingToken(Guid userId);
+    }
+}
