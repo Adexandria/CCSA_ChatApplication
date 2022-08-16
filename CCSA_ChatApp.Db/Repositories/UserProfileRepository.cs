6901116a51542bed724dce5f10fb3bcf1b9160ad
gideon <edoghotugiddy@gmail.com>
2022-08-16 18:46:46 +0100

    added the userProfileRepo

--- CCSA_ChatApp.Db/Repositories/UserProfileRepository.cs
+++ CCSA_ChatApp.Db/Repositories/UserProfileRepository.cs
@@ -14,6 +14,25 @@ namespace CCSA_ChatApp.Db.Repositories
 
         }
 
+        public void CreateExistingUserProfile(User user)
+        {
+            _session.Save(user);
+        }
 
+        public UserProfile? GetUserProfileById(Guid userProfileId)
+        {
+            var userProfile = _session.Query<UserProfile>().FirstOrDefault(x => x.ProfileId == userProfileId);
+            return userProfile;
+        }
+
+        public void DeleteUserProfileById(Guid userProfileId)
+        {
+            var userProfile = GetUserProfileById(userProfileId);
+            if (userProfileId != null)
+            {
+                _session.Delete(userProfileId);
+                Commit();
+            }
+        }
     }
 }
