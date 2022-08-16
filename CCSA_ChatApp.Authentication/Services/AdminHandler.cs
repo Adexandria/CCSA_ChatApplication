d56a1bac603eedd32b0e5aa265ce3ab9c25bf8d9
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-16 15:28:22 +0100

    fixed error

--- CCSA_ChatApp.Authentication/Services/AdminHandler.cs
+++ CCSA_ChatApp.Authentication/Services/AdminHandler.cs
@@ -8,13 +8,13 @@ namespace CCSA_ChatApp.Authentication.Services
     {
         protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, AdminRequirement requirement)
         {
-            var groupRole = context.User.Claims.Where(s=>s.Type==ClaimTypes.Role).Select(s=>s.Value.Contains(requirement.Role));
+            var groupRole = context.User.Claims.Where(s=>s.Type==ClaimTypes.Role && s.Value.Contains(requirement.Role)).Select(s=>s.Value).ToList();
             if(groupRole is null)
             {
                 return;
             }
             var authContext = context.Resource as HttpContext;
-            var groupName = authContext.Request.Query.Select(s => s.Key == "groupName").FirstOrDefault();
+            var groupName = authContext.Request.Query.Where(s => s.Key == "groupName").Select(s=>s.Value).FirstOrDefault();
             if (groupRole.Contains(groupName))
             {
               context.Succeed(requirement);
