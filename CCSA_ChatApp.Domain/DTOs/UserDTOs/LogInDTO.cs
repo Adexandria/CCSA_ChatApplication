e788d81b6461ebd42cb9532b3f1d282e28c31202
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-16 09:04:09 +0100

    created UserDTOs

---
+++ CCSA_ChatApp.Domain/DTOs/UserDTOs/LogInDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSA_ChatApp.Domain.DTOs.UserDTOs
+{
+    public class LogInDTO
+    {
+        [Required(ErrorMessage = "Enter Username")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Enter Password")]
+        public string Password { get; set; }
+    }
+}
---
+++ CCSA_ChatApp.Domain/DTOs/UserDTOs/PasswordDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CCSA_ChatApp.Domain.DTOs.UserDTOs
+{
+    public class PasswordDTO
+    {
+        [Required(ErrorMessage = "Enter Old Password")]
+        public string OldPassword { get; set; }
+
+        [Password, StringLength(7, ErrorMessage = "Password must be more than 4 and less than 7 characters", MinimumLength = 4)]
+        public string NewPassword { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password and ConfirmPassword do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
