1e05f0af491cae974d988e4194bfbac057450062
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-17 18:53:20 +0100

    created mapping service

---
+++ CCSA_ChatApp.Infrastructure/Services/MappingService.cs
@@ -0,0 +1,19 @@
+﻿using CCSA_ChatApp.Domain.DTOs.UserDTOs;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSA_ChatApp.Infrastructure.Services
+{
+    public class MappingService
+    {
+        public static TypeAdapterConfig UsersMappingService()
+        {
+            return TypeAdapterConfig<UserDTO, UsersDTO>.NewConfig().
+                Map(dest => dest.FullName, src => $"{src.FirstName} {src.MiddleName} {src.LastName}").Config;
+        }
+    }
+}
