534416822eac117e4de2b08b90d415685068e717
gideon <edoghotugiddy@gmail.com>
2022-08-17 20:07:10 +0100

    access token after creating the groupchat

--- CCSA_ChatApplication/Controllers/GroupChatController.cs
+++ CCSA_ChatApplication/Controllers/GroupChatController.cs
@@ -1,9 +1,12 @@
-﻿using CCSA_ChatApp.Domain.DTOs.GroupChatDTOs;
+﻿using CCSA_ChatApp.Authentication.Services;
+using CCSA_ChatApp.Domain.DTOs.GroupChatDTOs;
 using CCSA_ChatApp.Domain.Models;
 using CCSA_ChatApp.Infrastructure.Services;
+using Mapster;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CCSA_ChatApplication.Controllers
 {
@@ -13,21 +16,41 @@ namespace CCSA_ChatApplication.Controllers
     public class GroupChatController : ControllerBase
     {
 
-        public GroupChatController(IGroupChatService groupChatService, IAuth authService)
+        public GroupChatController(IGroupChatService groupChatService, IAuth authService, ITokenCredential tokenCredential, IUserService userService)
         {
             _groupChatService = groupChatService;
             _authService = authService;
+            _tokenCredential = tokenCredential;
+            _userService = userService;
         }
 
         public IGroupChatService _groupChatService { get; }
         public IAuth _authService { get; }
-
+        public ITokenCredential _tokenCredential { get; }
+        public IUserService _userService { get; }
 
         [HttpPost("create-group")]
-        public async Task<IActionResult> CreateGroupChat(NewGroupChatDTO newGroupChat)
+        public async Task<IActionResult> CreateGroupChat(NewGroupChatDTO newGroupChat, string accessToken)
         {
-            await _groupChatService.CreateGroupChat(newGroupChat);
-            return Ok($"{ newGroupChat.GroupName} Created");
+            try
+            {
+                await _groupChatService.CreateGroupChat(newGroupChat);
+                var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var user = _userService.GetUserById(Guid.Parse(userId)).Result.Adapt<User>();
+                var token = await _tokenCredential.GenerateToken(user, accessToken);
+                return Ok(token.AccessToken);
+                //return Ok($"{newGroupChat.GroupName} Created");
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
         }
+
+
+
+
+
     }
 }
