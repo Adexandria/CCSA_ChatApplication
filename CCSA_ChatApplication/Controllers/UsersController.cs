1004f5bd2c4829cd8f75a794da6e3bac1fbb886f
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-17 18:16:12 +0100

    added controller

---
+++ CCSA_ChatApplication/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using CCSA_ChatApp.Domain.DTOs.UserProfileDTOs;
+using CCSA_ChatApp.Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCSA_ChatApplication.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _usersService;
+
+        public UserController(IUserService usersService)
+        {
+            _usersService = usersService;
+        }
+
+   /*     [HttpGet("contacts")]
+        public async Task<UserProfileDTO> GetAllContacts()
+        {
+            
+        }*/
+
+    }
+}
