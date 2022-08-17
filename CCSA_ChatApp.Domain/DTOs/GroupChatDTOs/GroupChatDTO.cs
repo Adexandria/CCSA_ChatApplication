19ba224bb6434fb2058ff8a5ee3672d262c6870e
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-17 15:23:56 +0100

    updated

--- CCSA_ChatApp.Domain/DTOs/GroupChatDTOs/GroupChatDTO.cs
+++ CCSA_ChatApp.Domain/DTOs/GroupChatDTOs/GroupChatDTO.cs
@@ -6,13 +6,13 @@ namespace CCSA_ChatApp.Domain.DTOs.GroupChatDTOs
 {
     public class GroupChatDTO
     {
-        public virtual Guid GroupId { get; set; }
-        public virtual string GroupName { get; set; }
-        public virtual string GroupDescription { get; set; }
-        public virtual IFormFile Picture { get; set; }
-        public virtual DateTime CreatedDate { get; protected set; }
-        public virtual UserDTO CreatedBy { get; set; }
-        public virtual List<UsersDTO> Members { get; set; } 
-        public virtual List<MessageHistoryDTO> ChatHistory { get; set; } 
+        public  Guid GroupId { get; set; }
+        public  string GroupName { get; set; }
+        public  string GroupDescription { get; set; }
+        public  byte[] Picture { get; set; }
+        public DateTime CreatedDate { get; protected set; }
+        public  UserDTO CreatedBy { get; set; }
+        public  List<UsersDTO> Members { get; set; } 
+        public  List<MessageHistoryDTO> ChatHistory { get; set; } 
     }
 }
