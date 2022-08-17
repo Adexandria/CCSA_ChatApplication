dd2ddce0359e03516f598666793fe0d1d8e0fb96
Iruoma Onyia <Iruomaonyia@outlook.com>
2022-08-17 09:15:52 +0100

    Draft of IMessageService and IMessageHistoryService

---
+++ CCSA_ChatApp.Infrastructure/Services/IMessageHistoryService.cs
@@ -0,0 +1,16 @@
+﻿using CCSA_ChatApp.Domain.DTOs.MessageDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSA_ChatApp.Infrastructure.Services
+{
+    public interface IMessageHistoryService
+    {
+        List<MessageDTO> FetchMessagesBySenderId(Guid senderId);
+        List<MessageDTO> FetchMessagesByRecieverUsername(string recieverUsername);
+        List<MessageDTO> FetchGroupChatMessagesByGroupId(Guid groupId);
+    }
+}
---
+++ CCSA_ChatApp.Infrastructure/Services/IMessageService.cs
@@ -0,0 +1,20 @@
+﻿using CCSA_ChatApp.Domain.DTOs.MessageDTOs;
+using CCSA_ChatApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCSA_ChatApp.Infrastructure.Services
+{
+    public interface IMessageService
+    {
+        void SendMessage(string text, string senderUsername, string receiverUsername);
+        void SendMessageToGroup(string text, string senderUsername, Guid groupChatId);
+        void UpdateMessage(string text);
+        void DeleteMessageByMessageId(Guid messageId);
+        
+
+    }
+}
