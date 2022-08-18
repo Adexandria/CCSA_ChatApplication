f02d92dcdbcf6cff7e9ed8b06876519d1cb40ac2
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-18 12:12:19 +0100

    Fixed issue

--- CCSA_ChatApp.Domain/DTOs/UserProfileDTOs/UserProfileDTO.cs
+++ CCSA_ChatApp.Domain/DTOs/UserProfileDTOs/UserProfileDTO.cs
@@ -11,7 +11,7 @@ namespace CCSA_ChatApp.Domain.DTOs.UserProfileDTOs
         public string Email { get; set; }
         public List<UsersDTO> Contacts { get; set; }
         public virtual string Country { get; set; }
-        public List<GroupChatDTO> GroupChats { get; set; }
+        public List<GroupChatsDTO> GroupChats { get; set; }
         public virtual string Username { get; set; }
         public virtual string Picture { get; set; }
         
